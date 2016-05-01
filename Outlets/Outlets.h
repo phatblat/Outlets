34621a3ac4685b4b453df684199b24ca08eb716f
Ben Chatelain <ben@octop.ad>
2016-05-01 12:13:23 -0600

    Add iOS framework project template

---
+++ Outlets/Outlets.h
@@ -0,0 +1,19 @@
+//
+//  Outlets.h
+//  Outlets
+//
+//  Created by Ben Chatelain on 5/1/16.
+//  Copyright © 2016 Ben Chatelain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Outlets.
+FOUNDATION_EXPORT double OutletsVersionNumber;
+
+//! Project version string for Outlets.
+FOUNDATION_EXPORT const unsigned char OutletsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Outlets/PublicHeader.h>
+
+
